a3a1c82020c49e40334a9853f293bb0aedac68a0
Marved <108392678+MarvedCodes@users.noreply.github.com>
2023-02-09 11:46:41 +0800

    Create helper functions for form validation

---
+++ utils/index.js
@@ -0,0 +1,9 @@
+export const validateEmail = email => {
+  return email.match(
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  );
+};
+
+export const validateName = name => {
+  return name.match(/^[a-zA-Z]+$/);
+};
